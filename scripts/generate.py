7d275b39ed63fd42583a65cf57d543e236ebbfa9
josephTaiv <joseph@taiv.tv>
2025-09-13 19:28:01 -0400

    ceberas working

---
+++ scripts/generate.py
@@ -0,0 +1,15 @@
+import os
+from cerebras.cloud.sdk import Cerebras
+
+client = Cerebras(
+  api_key="csk-w4mcxrcex2vkpv24jdtv23kyjyh9jyn5cxw2terkp9k2xtxn"
+)
+
+chat_completion = client.chat.completions.create(
+  messages=[
+  {"role": "user", "content": "Why is fast inferepoetry run nce important?",}
+],
+  model="llama-4-scout-17b-16e-instruct",
+)
+
+print(chat_completion.choices[0].message.content)
